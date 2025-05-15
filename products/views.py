adb80ff81d7debfd8bafc67e42c8141bca81ad40
gautam jain <gautamjain@gautams-MacBook-Air.local>
2025-05-15 17:17:55 +0100

    Products Urls set

--- products/views.py
+++ products/views.py
@@ -1,28 +1,19 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from .models import Product
-from .serializers import ProductSerializer
-
-# class ProductViewSet(viewsets.ModelViewSet):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
-#     permission_classes = [IsAuthenticated]
-
-#     def perform_create(self, serializer):
-#         serializer.save()  # or .save(user=self.request.user) if user field exists
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListCreateView(APIView):
+class ProductListView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         products = Product.objects.filter(user=request.user)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+class ProductCreateView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
@@ -31,19 +22,13 @@ class ProductListCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-class ProductDetailView(APIView):
+class ProductEditView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self, pk, user):
-        try:
-            return Product.objects.get(pk=pk, user=user)
-        except Product.DoesNotExist:
-            return None
-
     def put(self, request, pk):
-        product = self.get_object(pk, request.user)
-        if not product:
+        try:
+            product = Product.objects.get(pk=pk, user=request.user)
+        except Product.DoesNotExist:
             return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = ProductSerializer(product, data=request.data)
@@ -52,9 +37,13 @@ class ProductDetailView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class ProductDeleteView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def delete(self, request, pk):
-        product = self.get_object(pk, request.user)
-        if not product:
+        try:
+            product = Product.objects.get(pk=pk, user=request.user)
+        except Product.DoesNotExist:
             return Response({"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND)
 
         product.delete()
