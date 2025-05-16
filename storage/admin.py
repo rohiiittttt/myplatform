6a485384f78f6a8ea44da92f89979ad54b3faa3d
rohit kerkar <129620973+rohiiittttt@users.noreply.github.com>
2025-05-16 16:00:52 +0530

    storage admin access

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import StorageProvider
 
-
+@admin.register(StorageProvider)
+class StorageProviderAdmin(admin.ModelAdmin):
+    list_display = ('storage_location', 'storage_capacity', 'current_occupancy', 'status', 'user')
